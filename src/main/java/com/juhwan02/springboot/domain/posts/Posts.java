a35fad3c948ab50e6d6e4c84981ab0eb55188a82
juhwan <ngcps2@naver.com>
2021-06-06 23:42:23 +0900

    테스트

---
+++ src/main/java/com/juhwan02/springboot/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.juhwan02.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Getter //롬북 어노테이션
+@NoArgsConstructor//롬북 어노테이션
+@Entity //JPA 어노테이션
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+}
---
+++ src/main/java/com/juhwan02/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.juhwan02.springboot.web;
+
+import com.juhwan02.springboot.web.dto.HelloResponseDto;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
+    }
+}
